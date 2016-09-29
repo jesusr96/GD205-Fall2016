dff9afd26d92a8b442ea54ecff9fdf1b05812d9a
jesusr96 <student@c515-d25hq0ncdhjt-0042012.hostos.cuny.edu>
2016-09-29 13:37:01 -0400

    code exercise 2 scripts

---
+++ scripts/rotate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Rotate the object around its local X axis at 1 degree per second
+		transform.Rotate(Time.deltaTime, -5, 0);
+
+		// ...also rotate around the World's Y axis
+		transform.Rotate(5, Time.deltaTime, 10, Space.World);
+
+		transform.RotateAround(Vector3.zero, Vector3.up, -40 * Time.deltaTime);
+	}
+}
